da23e77709699c6e359f1361be2314370f98cf82
Tux <write@imaginarycode.com>
2014-10-05 15:15:24 -0400

    Add ColoCrossing, shit host

--- sim.rb
+++ sim.rb
@@ -11,6 +11,7 @@ options = Hash.new
 options["Host from your home computer. (-$0 per month)"] = Proc.new{ app.add_component HomeHost.new }
 options["Order an OVH server. (-$42 per month)"] = Proc.new{ app.add_component OVH.new },
 options["Order a datashack server. (-$34 per month)"] = Proc.new{ app.add_component Datashack.new }
+options["Order a ColoCrossing server. (-$40 per month)"] = Proc.new{ app.add_component Colocrossing.new }
 
 app.draw "You are 14 years old and want to start a hosting company. You have $100. How do you begin?", options
 
